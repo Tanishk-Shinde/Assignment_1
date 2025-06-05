d1c0370fdda7c3e5e58edc9e4d6ce697d9c5f9e6
Tanishk-Shinde <myasus528@gmail.com>
2025-06-05 10:34:55 +0530

    i learned functions

---
+++ function.js
@@ -0,0 +1,23 @@
+// function sq(x)
+// {
+//     let ans = 0;
+//     ans = x*x;
+//     return ans;
+// }
+// console.log("Final answer:"+ sq(5));
+
+// const sq = function(x)
+// {
+//     let ans = 0;
+//     ans = x*x;
+//     return ans;
+// }
+// console.log("final answer: " + sq(2));
+
+const sq = function(x=6)
+{
+    let ans = 0;
+    ans = x*x;
+    return ans;
+}
+console.log("final answer: " + sq());
