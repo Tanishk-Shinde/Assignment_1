16fd2fd92a0091169797793653bf37019a177f74
Tanishk-Shinde <myasus528@gmail.com>
2025-06-05 11:16:30 +0530

    inplmented callbacks

--- function.js
+++ function.js
@@ -1,3 +1,6 @@
+
+//function with argument
+
 // function sq(x)
 // {
 //     let ans = 0;
@@ -6,7 +9,9 @@
 // }
 // console.log("Final answer:"+ sq(5));
 
-// const sq = function(x)
+//function with parameter and arguments
+
+// const sq = function(4)
 // {
 //     let ans = 0;
 //     ans = x*x;
@@ -14,10 +19,39 @@
 // }
 // console.log("final answer: " + sq(2));
 
-const sq = function(x=6)
-{
-    let ans = 0;
-    ans = x*x;
-    return ans;
-}
-console.log("final answer: " + sq());
+//function with parameter
+
+// const sq = function(x=6)
+// {
+//     let ans = 0;
+//     ans = x*x;
+//     return ans;
+// }
+// console.log("final answer: " + sq());
+
+//Callback functions 
+
+// function hello(name, fn1)
+// {
+//     console.log("Hello, " + name );
+// fn1();
+// }
+// function bye()
+// {
+//     console.log("GoodByee");
+// }
+// hello("Tanishk", bye);
+
+// function color(cname, cfunction)
+// {
+//     console.log("Hello, "+ cname);
+// cfunction();
+// }
+// function cl()
+// {
+//     console.log("Nice name...");
+// }
+// color("Tanishk", cl);
+
+//Arrow function
+
